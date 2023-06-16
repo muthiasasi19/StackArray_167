16ed5e109a2bb65b107d97d241351dc92d176e39
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-06-16 00:43:21 -0700

    Stack Linked List

--- StackLinkedList/StackLinkedList.cpp
+++ StackLinkedList/StackLinkedList.cpp
@@ -6,6 +6,23 @@ using namespace std;
 struct Node {
     int data;
     Node* next;
+
+    // Kelas Stack
+    class StackLinkedList {
+    private:
+        Node* top; // Pointer ke elemen teratas stack
+
+    public:
+        // Constructor
+        StackLinkedList() {
+            top = NULL;
+        }
+
+        // Fungsi untuk mengecek isi data stack 
+        bool isEmpty() {
+            return (top == NULL);
+        }
+
 };
 
 
