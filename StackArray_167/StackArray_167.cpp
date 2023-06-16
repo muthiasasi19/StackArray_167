3439791e55cccaf102b6529c223fe7c575950cd7
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-06-15 23:37:38 -0700

    membuat fungsi push

--- StackArray_167/StackArray_167.cpp
+++ StackArray_167/StackArray_167.cpp
@@ -12,4 +12,20 @@ public:
 	int stackArray() {
 		top = -1;
 	}
+
+	void push() {
+		cout << "\nEnter an Element:";
+		int element;
+		cin >> element;
+
+		if (top == 4) {
+			cout << "Number of data exceeds the limit." << endl;
+			return;
+		}
+		top++;
+		stack_array[top] = element; // step 3
+		cout << endl;
+		cout << element << "Ditambahkan(pushed)" << endl;
+	} 
+
 };
